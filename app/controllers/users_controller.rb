5165d66afeb3085489936cbed5a8169f143e9520
j0ker <b8d@yandex.ru>
2011-10-02 10:57:12 +0000

    Create Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,33 +3,18 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render :json => @users }
-    end
   end
 
   # GET /users/1
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render :json => @user }
-    end
   end
 
   # GET /users/new
   # GET /users/new.json
   def new
     @user = User.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render :json => @user }
-    end
   end
 
   # GET /users/1/edit
@@ -81,3 +66,4 @@ class UsersController < ApplicationController
     end
   end
 end
+
